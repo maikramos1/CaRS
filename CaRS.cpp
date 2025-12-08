100a7b16cf7aa2d266005a7078ce6918a895a8b1
Maik Ramos <maik.ramos@optsolv.com.br>
2025-12-08 16:56:57 -0300

    criacao das instancias

--- CaRS.cpp
+++ CaRS.cpp
@@ -3,7 +3,21 @@
 
 int main(void) {
 
-	
+
+	leArquivoNaoEuclideano();
 
 	return 0;
+}
+
+void leArquivoNaoEuclideano(const char* caminho) {
+	FILE* arquivo = fopen(caminho, "r");
+	if (arquivo == NULL) {
+		printf("Erro ao abrir o arquivo: %s\n", caminho);
+		return;
+	}
+	char buffer[256];
+	while (fgets(buffer, sizeof(buffer), arquivo) != NULL) {
+		printf("%s", buffer);
+	}
+	fclose(arquivo);
 }
--- CaRS.h
+++ CaRS.h
@@ -1,1 +1,17 @@
-#pragma once
+﻿#pragma once
+#ifndef cabecalho
+#define cabecalho
+
+#define MAX_CID 20
+#define MAX_CAR	10
+#define MAX_NOME 200
+
+char nome[MAX_NOME];
+int num_cidades;
+int num_carros;
+int mat_distancia[MAX_CID][MAX_CID];
+int mat_retorno[MAX_CID][MAX_CID];
+
+void leArquivoNaoEuclideano(const char* caminho);
+
+#endif // cabecalho
