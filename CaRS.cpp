eb3099970cce9e206ba0097a6acf667cf8377abf
Maik Ramos <maik.ramos@optsolv.com.br>
2025-12-08 17:08:05 -0300

    inicio de confecção da leitura de arquivos não euclideanos

--- CaRS.cpp
+++ CaRS.cpp
@@ -4,20 +4,54 @@
 int main(void) {
 
 
-	leArquivoNaoEuclideano();
+	leArquivoNaoEuclideano("Mautitania10n.car");
+	imprimeArquivoNaoEuclideano();
 
 	return 0;
 }
 
 void leArquivoNaoEuclideano(const char* caminho) {
-	FILE* arquivo = fopen(caminho, "r");
-	if (arquivo == NULL) {
+	
+	FILE* f = fopen(caminho, "r");
+	
+	char aux[200];
+
+	if (f == NULL) {
 		printf("Erro ao abrir o arquivo: %s\n", caminho);
 		return;
 	}
-	char buffer[256];
-	while (fgets(buffer, sizeof(buffer), arquivo) != NULL) {
-		printf("%s", buffer);
-	}
-	fclose(arquivo);
+	// Leitura do nome
+	fscanf(f, "%s[7]", aux, &nomeInstancia);
+	
+	// joga fora tipo e comentario 
+	fscanf(f, "%s", aux);
+	fscanf(f, "%s", aux);
+
+	// Leitura do numero de cidades
+	fscanf(f, "%s %d", aux, &num_cidades);
+
+	// Leitura do numero de carros
+	fscanf(f, "%s %d", aux, &num_carros);
+
+	// joga fora tipo, formato e seção das arestas
+	fscanf(f, "%s", aux);
+	fscanf(f, "%s", aux);
+	fscanf(f, "%s", aux);
+
+	/*
+	for (int j = 0; j < num_obj; j++)
+		fscanf(f, "%d", &vet_val_obj[j]);
+
+	for (int j = 0; j < num_obj; j++)
+		fscanf(f, "%d", &vet_pes_obj[j]);
+
+	for (int i = 0; i < num_moc; i++)
+		fscanf(f, "%d", &vet_cap_moc[i]);
+	*/
+
+	fclose(f);
+}
+
+void imprimeArquivoNaoEuclideano() {
+	printf("Nome da Instancia: %s\n", nomeInstancia);
 }
--- CaRS.h
+++ CaRS.h
@@ -6,12 +6,13 @@
 #define MAX_CAR	10
 #define MAX_NOME 200
 
-char nome[MAX_NOME];
+char nomeInstancia[MAX_NOME];
 int num_cidades;
 int num_carros;
 int mat_distancia[MAX_CID][MAX_CID];
 int mat_retorno[MAX_CID][MAX_CID];
 
 void leArquivoNaoEuclideano(const char* caminho);
+void imprimeArquivoNaoEuclideano();
 
 #endif // cabecalho
