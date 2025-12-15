d823028b841cababa7cb598c73d92134fb656e68
Maik <maik.maifredo@edu.ufes.br>
2025-12-15 01:15:00 -0300

    Finalização das restrições e inicio da função de otimização do cplex

--- CaRS.cpp
+++ CaRS.cpp
@@ -35,7 +35,15 @@ int main(void) {
 	
 	leArquivoNaoEuclideano(entrada[instancia]);
 	
-	escreveCEPLEX(saida[instancia]);
+	//escreveCEPLEX(saida[instancia]);
+	escreveCEPLEX(" ");
+
+	//const char* modelo = "Pequenas/Mauritania10n.lp";
+	//const char* instancia2 = "teste";
+	//const char* arq_solucao = "teste.txt";
+	//const char* arq_res_obtidos = "resultados_obtidos.csv";
+
+	//optimize_cplex(modelo, instancia2, arq_solucao, arq_res_obtidos);
 
 	//imprimeArquivoNaoEuclideano(" ");
 
@@ -342,10 +350,202 @@ void escreveCEPLEX(const char* arq) {
 	}
 	fprintf(f, " >= 1\n");
 	
-	//8ª Restrição
-	//for (int i = 2; i < n)
+	//8ª Restrição sendo dividida em 8.1 e 8.2
+	//8.1ª Restrição
+	for (int i = 1; i < num_cidades; i++) fprintf(f, "u_%d >= 2\n", i);
 
+	//8.2ª Restrição
+	for (int i = 1; i < num_cidades; i++) fprintf(f, "u_%d <= %d\n", i, num_cidades-1);
+
+	//9ª Restrição
+	//restrição maldita da desgaça
+	for (int i = 1; i < num_cidades; i++) {
+		for (int j = 1; j < num_cidades; j++) {
+			fprintf(f, "u_%d - u_%d + 1 - %d", i, j, num_cidades - 2);
+			for(int c = 0; c < num_carros; c++) {
+				fprintf(f, " + %d f_%d_%d_%d", num_cidades - 2, c, i, j);
+			}
+			fprintf(f, " <= 0\n");
+		}
+	}
+
+	//Antes da 10ª Restrição, declaro o bounds
+	fprintf(f, "Bounds\n");
+	for (int i = 1; i < num_cidades; i++) {
+		fprintf(f, "u_%d >= 1\n", i);
+	}
+	
+	//10ª Restrição
+	fprintf(f,"BIN\n");
+	for (int c = 0; c < num_carros; c++) {
+		for (int i = 0; i < num_cidades; i++) {
+			for (int j = 0; j < num_cidades; j++) {
+				if (j == 0) {
+					fprintf(f, "f_%d_%d_%d", c, i, j);
+				}
+				else {
+					fprintf(f, " f_%d_%d_%d", c, i, j);
+				}
+			}
+		}
+	}
+	fprintf(f, "\n");
+	//suposição que no artigo está incorreto
+	for (int c = 0; c < num_carros; c++) {
+		for (int i = 0; i < num_cidades; i++) {
+			for (int j = 0; j < num_cidades; j++) {
+				if (j == 0) {
+					fprintf(f, "w_%d_%d_%d", c, i, j);
+				}
+				else {
+					fprintf(f, " w_%d_%d_%d", c, i, j);
+				}
+			}
+		}
+	}
+	fprintf(f, "\n");
+	//suposição que no artigo está incorreto também
+	for (int c = 0; c < num_carros; c++) {
+		for (int i = 0; i < num_cidades; i++) {
+			if (i == 0) {
+				fprintf(f, "a_%d_%d", c, i);
+			}
+			else {
+				fprintf(f, " a_%d_%d", c, i);
+			}
+		}
+	}
+	fprintf(f, "\n");
+	for (int c = 0; c < num_carros; c++) {
+		for (int i = 0; i < num_cidades; i++) {
+			if (i == 0) {
+				fprintf(f, "e_%d_%d", c, i);
+			}
+			else {
+				fprintf(f, " e_%d_%d", c, i);
+			}
+		}
+	}
+	fprintf(f, "\n");
+
+	//11ª Restrição
+	//Generals
+	fprintf(f, "Gen\n");
+	for (int i = 1; i < num_cidades; i++) {
+		if (i == 1) {
+			fprintf(f, "u_%d", i);
+		}
+		else {
+			fprintf(f, " u_%d", i);
+		}
+	}
+	fprintf(f, "\n");
+
+	fprintf(f, "END");
 
 	if (strcmp(arq, " "))
 		fclose(f);
+}
+
+void optimize_cplex(const char* modelo, const char* instancia, const char* arq_solucao, const char* arq_res_obtidos) {
+	int sts = 0;
+	double sol = 0.0;
+	double lb, gap, tempo_ini, tempo_fim, tempo;
+
+	CPXENVptr env = NULL;
+	CPXLPptr lp = NULL;
+
+	env = CPXopenCPLEX(&sts);
+	if (env == NULL) {
+		fprintf(stderr, "Nao foi possivel abrir o ambiente CPLEX. (status code %d)\n", sts);
+		exit(1);
+	}
+
+	sts = CPXsetintparam(env, CPX_PARAM_SCRIND, CPX_ON);
+	check_status_cplex(env, sts, "CPXsetintparam(SCRIND)");
+
+	sts = CPXsetdblparam(env, CPX_PARAM_TILIM, 3600.0);
+	check_status_cplex(env, sts, "CPXsetdblparam(TILIM)");
+
+	lp = CPXcreateprob(env, &sts, "meu_problema_lp");
+	check_status_cplex(env, sts, "CPXcreateprob");
+
+	sts = CPXreadcopyprob(env, lp, modelo, NULL);
+	check_status_cplex(env, sts, "CPXreadcopyprob");
+
+	sts = CPXgettime(env, &tempo_ini);
+	check_status_cplex(env, sts, "CPXgettime(start)");
+
+	sts = CPXmipopt(env, lp);
+	check_status_cplex(env, sts, "CPXmipopt");
+
+	sts = CPXgettime(env, &tempo_fim);
+	check_status_cplex(env, sts, "CPXgettime(end)");
+
+	tempo = tempo_fim - tempo_ini;
+
+	sts = CPXgetobjval(env, lp, &sol);
+	check_status_cplex(env, sts, "CPXgetobjval");
+
+	sts = CPXgetbestobjval(env, lp, &lb);
+	check_status_cplex(env, sts, "CPXgetbestobjval");
+
+	sts = CPXgetmiprelgap(env, lp, &gap);
+	check_status_cplex(env, sts, "CPXgetmiprelgap");
+
+	int num_vars = CPXgetnumcols(env, lp);
+
+	if (num_vars > 0) {
+		double* var_values = new double[num_vars];
+		char** var_names = new char* [num_vars];
+		int names_buf_size;
+		int surplus;
+		char* names_buf = NULL;
+
+		sts = CPXgetmipx(env, lp, var_values, 0, num_vars - 1);
+		check_status_cplex(env, sts, "CPXgetmipx");
+
+		sts = CPXgetcolname(env, lp, NULL, NULL, 0, &surplus, 0, num_vars - 1);
+		if (sts != CPXERR_NEGATIVE_SURPLUS && sts != 0) {
+			check_status_cplex(env, sts, "CPXgetcolname (chamada 1)");
+		}
+
+		names_buf_size = -surplus;
+
+		if (names_buf_size > 0) {
+			names_buf = new char[names_buf_size];
+
+			sts = CPXgetcolname(env, lp, var_names, names_buf, names_buf_size, &surplus, 0, num_vars - 1);
+			check_status_cplex(env, sts, "CPXgetcolname (chamada 2)");
+
+			//escrever_relatorio_solucao(arq_solucao, sol, num_vars, var_values, var_names);
+
+			FILE* f = fopen(arq_res_obtidos, "a");
+
+			fprintf(f, "%s;%.0f;%.0f;%.4f;%.4f;", instancia, lb, sol, gap, tempo);
+
+			fclose(f);
+		}
+
+		delete[] var_values;
+		if (names_buf != NULL) {
+			delete[] names_buf;
+		}
+		delete[] var_names;
+	}
+}
+
+void check_status_cplex(CPXENVptr env, int sts, const char* function_name) {
+	if (sts) {
+		char error_string[CPXMESSAGEBUFSIZE];
+
+		CPXgeterrorstring(env, sts, error_string);
+		fprintf(stderr, "ERRO DO CPLEX na funcao %s: %s (status code %d)\n", function_name, error_string, sts);
+
+		if (env) {
+			CPXcloseCPLEX(&env);
+		}
+
+		exit(1);
+	}
 }
--- CaRS.h
+++ CaRS.h
@@ -2,6 +2,8 @@
 #ifndef cabecalho
 #define cabecalho
 
+#include "C:\Program Files\IBM\ILOG\CPLEX_Studio_Community2212\opl\include\ilcplex\cplex.h"
+
 #define MAX_CID 20
 #define MAX_CAR	10
 #define MAX_NOME 200
@@ -15,5 +17,8 @@ int mat_retorno[MAX_CAR][MAX_CID][MAX_CID];
 void leArquivoNaoEuclideano(const char* caminho);
 void imprimeArquivoNaoEuclideano(const char * arq);
 void escreveCEPLEX(const char* arq);
+
+void optimize_cplex(const char* modelo, const char* instancia, const char* arq_solucao, const char* arq_res_obtidos);
+void check_status_cplex(CPXENVptr env, int sts, const char* function_name);
 
 #endif // cabecalho
